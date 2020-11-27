b64ec85cd9b2db6a54bd519edee2db018f02adce
Joyce Chen <64048278+winterberry123@users.noreply.github.com>
2020-11-27 12:13:15 -0800

    Update imageslider.js

--- assets/js/imageslider.js
+++ assets/js/imageslider.js
@@ -1,6 +1,27 @@
+// var slideIndex = [1,1];
+// var slideId = ["mySlides2"]
+// showSlides(1, 0);
+
+// function plusSlides(n, no) {
+//   showSlides(slideIndex[no] += n, no);
+// }
+
+// function showSlides(n, no) {
+//   var i;
+//   var x = document.getElementsByClassName(slideId[0]);
+//   if (n > x.length) {slideIndex[no] = 1}    
+//   if (n < 1) {slideIndex[no] = x.length}
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";  
+//   }
+//   x[slideIndex[no]-1].style.display = "block";  
+// }
+
+
 var slideIndex = [1,1];
-var slideId = ["mySlides2"]
+var slideId = ["mySlides1", "mySlides2"]
 showSlides(1, 0);
+showSlides(1, 1);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
@@ -8,7 +29,7 @@ function plusSlides(n, no) {
 
 function showSlides(n, no) {
   var i;
-  var x = document.getElementsByClassName(slideId[0]);
+  var x = document.getElementsByClassName(slideId[no]);
   if (n > x.length) {slideIndex[no] = 1}    
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
