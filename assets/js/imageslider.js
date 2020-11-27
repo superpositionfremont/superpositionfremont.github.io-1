e421f09411fc4527b94850a3476531c76e4e0b62
Joyce Chen <64048278+winterberry123@users.noreply.github.com>
2020-11-26 23:16:51 -0800

    Update imageslider.js

--- assets/js/imageslider.js
+++ assets/js/imageslider.js
@@ -1,7 +1,6 @@
 var slideIndex = [1,1];
 var slideId = ["mySlides2"]
 showSlides(1, 0);
-showSlides(1, 1);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
@@ -9,7 +8,7 @@ function plusSlides(n, no) {
 
 function showSlides(n, no) {
   var i;
-  var x = document.getElementsByClassName(slideId[no]);
+  var x = document.getElementsByClassName(slideId[0]);
   if (n > x.length) {slideIndex[no] = 1}    
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
